fada396f0824e6b08c9a02701490dfaa71eaea69
SaraCastril1 <smcastril1@eafit.edu.co>
2023-08-28 15:51:19 -0500

    Producer

---
+++ Reto2/Solucion-DEFINITIVO/API_Gateway/producer_grpc.py
@@ -0,0 +1,29 @@
+import grpc 
+import os
+import file_pb2
+import file_pb2_grpc
+#PRODUCER
+
+
+HOST = '[srv_persistor]:50051'
+
+def serve():
+	#Codigo estra -------------------------------------------------
+	
+	#source = os.environ['HOST']
+	#--------------------------------------------------------------
+	
+    #Aqui se debe levantar la conexion
+    with grpc.insecure_channel('srv_persistor:50051') as channel:
+        stub = file_pb2_grpc.FileStub(channel)
+        #request = monedas_pb2.EmptyMessage()
+        #response = stub.PingMonedas(request)
+        #print("Recived: ", response.ack)
+        request = file_pb2.file_request(file = "1")
+        response = stub.Find_file(request)
+        print("Recived: ", response.file)
+			
+
+
+if __name__ == "__main__":
+	serve()
