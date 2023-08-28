1abede3ba2d912faadc908548306e615994154d7
SaraCastril1 <smcastril1@eafit.edu.co>
2023-08-28 15:09:41 -0500

    Consumer

--- Reto2/Solucion/API_Gateway/main.py
+++ Reto2/Solucion/API_Gateway/main.py
@@ -1,28 +1,39 @@
-import grpc
 from concurrent import futures
+
+import grpc
 import monedas_pb2
 import monedas_pb2_grpc
+#CONSUMER
 
-#Genera un virtual env local para tener las dependencias instaladas -> Crealo dentro de srv_persistor
-
-def main():
-    #Numero de workers por defecto = 10
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10)) 
-
-    #Después de generar los ficheros pb2, los usaremos.
-    # 1. Importalos
-    # 2. Escuchar peticiones del
-    monedas_pb2_grpc.add_MonedasServicer_to_server(monedas_pb2_grpc.Monedas(), server)
-
-    # 3. Arrancar el servidor -> Cualquier IP en el puerto 50051
-    server.add_insecure_port('[::]:50051')
-    print("Service persistor is running... ")
-    server.start()
-    server.wait_for_termination()
-    
+HOST = '[::]:50051'
 
 
+def serve():
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        monedas_pb2_grpc.add_MonedasServicer_to_server(Monedas(), server)
+        server.add_insecure_port(HOST)
+        print("Service persistor is running... ")
+        server.start()
+        server.wait_for_termination()
+
+#Link el microservicio con una clase de python
+class Monedas(monedas_pb2_grpc.MonedasServicer):
+      
+      def PingMonedas(self, request, context):
+            #creamos un objeto respuesta
+            response = monedas_pb2.PingMonedasResponse(ack='1')
+            return response
+      
+      def SendMoneda(self, request, context):
+            print(request.date)
+            #print(request.source)
+            response = monedas_pb2.Moneda_response(data=request.open)
+            return response
+   
+#    def AddProduct(self, request, context):
+#       print("Request is received: " + str(request))
+#       return Service_pb2.TransactionResponse(status_code=1)
 
 
 if __name__ == "__main__":
-    main()
+    serve()
