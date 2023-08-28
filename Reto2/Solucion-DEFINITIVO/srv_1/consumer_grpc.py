d94796b88d9fdfb257b188fe9fb29186ad925f17
SaraCastril1 <smcastril1@eafit.edu.co>
2023-08-28 15:07:55 -0500

    Consumer

---
+++ Reto2/Solucion-DEFINITIVO/srv_1/consumer_grpc.py
@@ -0,0 +1,39 @@
+from concurrent import futures
+
+import grpc
+import file_pb2
+import file_pb2_grpc
+#CONSUMER
+
+HOST = '[::]:50051'
+
+
+def serve():
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        file_pb2_grpc.add_FileServicer_to_server(File(), server)
+        server.add_insecure_port(HOST)
+        print("Service find/list is running... ")
+        server.start()
+        server.wait_for_termination()
+
+#Link el microservicio con una clase de python
+class File(file_pb2_grpc.FileServicer):
+      
+      def Find_file(self, request, context):
+            print(request.file)
+            response = file_pb2.file_response(file='1')
+            return response
+      
+    #   def List_file(self, request, context):
+    #         print(request.date)
+    #         #print(request.source)
+    #         response = monedas_pb2.Moneda_response(data=request.open)
+    #         return response
+   
+#    def AddProduct(self, request, context):
+#       print("Request is received: " + str(request))
+#       return Service_pb2.TransactionResponse(status_code=1)
+
+
+if __name__ == "__main__":
+    serve()
