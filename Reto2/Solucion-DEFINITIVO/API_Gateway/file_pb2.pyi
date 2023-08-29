4dc443849f4da030f04646aa4013d5f52c7ad297
Ubuntu <ubuntu@ip-172-31-43-209.ec2.internal>
2023-08-29 00:09:52 +0000

    MAKE

---
+++ Reto2/Solucion-DEFINITIVO/API_Gateway/file_pb2.pyi
@@ -0,0 +1,17 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class file_request(_message.Message):
+    __slots__ = ["file"]
+    FILE_FIELD_NUMBER: _ClassVar[int]
+    file: str
+    def __init__(self, file: _Optional[str] = ...) -> None: ...
+
+class file_response(_message.Message):
+    __slots__ = ["file"]
+    FILE_FIELD_NUMBER: _ClassVar[int]
+    file: str
+    def __init__(self, file: _Optional[str] = ...) -> None: ...
