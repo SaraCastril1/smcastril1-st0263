95e1f00e67fe77d706370f0421430877c9c6cfa2
SaraCastril1 <smcastril1@eafit.edu.co>
2023-08-30 01:05:30 -0500

    rabbitMQ

--- Reto2/Solucion-DEFINITIVO/srv_2/consumer_rabbitMQ.py
+++ Reto2/Solucion-DEFINITIVO/srv_2/consumer_rabbitMQ.py
@@ -3,12 +3,44 @@
 # Consume RabbitMQ queue
 
 import pika
+import os
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('54.85.196.208', 5672, '/', pika.PlainCredentials("user", "password")))
-channel = connection.channel()
+        
+# def Find_file(self, request, context):
+#     file_path = os.path.join(request.file)
+#     print("Find: ",file_path)
+#     if os.path.exists(file_path):
+#         return file_pb2.file_response(file= 1, coincidence = [file_path])
+#     else:
+#         #return file_pb2.file_response(file= 0, coincidence = 'File not found')
+#         matching_files = fnmatch.filter(os.listdir("."), os.path.basename(file_path))
+#     if matching_files:
+#         return file_pb2.file_response(file=1, coincidence = matching_files)
+#     else:
+#         return file_pb2.file_response(file= 0, coincidence = ["File not found -> No coincidences"])
+                
 
-def callback(ch, method, properties, body):
-    print(f'{body} is received')
-    
-channel.basic_consume(queue="my_app", on_message_callback=callback, auto_ack=True)
-channel.start_consuming()
+# def List_file(self, request, context):
+#     try:
+#         files = os.listdir(request.file)
+#         print("List: ", request.file)
+#         return file_pb2.list_response(file = files)
+#     except OSError as e:
+#         return f"Error listing files in '{request.file}': {e}"
+#         #return []
+
+def main():
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters('54.85.196.208', 5672, '/', pika.PlainCredentials("user", "password")))
+    channel = connection.channel()
+
+    def callback(ch, method, properties, body):
+        print(f'{body} is received')
+        
+    channel.basic_consume(queue="my_app", on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
+
+
+
+if __name__ == "__main__":
+    main()
