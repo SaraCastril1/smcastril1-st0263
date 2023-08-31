6d74b17c12ae0984074d4f86d97fdcfca9da8dfa
SaraCastril1 <smcastril1@eafit.edu.co>
2023-08-30 21:08:01 -0500

    Flask

--- Reto2/Solucion-DEFINITIVO/API_Gateway/producer_REST.py
+++ Reto2/Solucion-DEFINITIVO/API_Gateway/producer_REST.py
@@ -18,7 +18,9 @@ def list_files(directory):
     with grpc.insecure_channel(f"{PRODUCER_HOST}:{PRODUCER_PORT}") as channel:
         stub = file_pb2_grpc.FileStub(channel)
         response = stub.List_file(file_pb2.file_request(file=directory))
-        return response
+        return response.file
+    
+
 
 @app.route('/find/<pattern>', methods=['GET'])
 def find_route(pattern):
@@ -29,6 +31,7 @@ def find_route(pattern):
 def list_route(directory):
     response = list_files(directory)
     return f"List result: {response.file}"
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
